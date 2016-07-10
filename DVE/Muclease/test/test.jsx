a2dce97e412700087a677cb5ddb7706d6f6cd3f3
nullice <ui@nullice.com>
2016-07-10 16:53:02 +0800

    test
    test

---
+++ DVE/Muclease/test/test.jsx
@@ -0,0 +1,86 @@
+﻿$.evalFile(File($.fileName).path + "/../Muclease_lib.jsx");
+var mu = new Muclease();
+
+
+// task -------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+// base --------------------------------------------
+
+var debug_log = "";
+
+//输出信息到控制台并记录到日志，以供使用 logSave() 输出到文件
+function log(info)
+{
+    $.writeln(info);
+    debug_log = debug_log +info +"\n";
+}
+
+
+function logSave()
+{
+    var f = new File(File($.fileName).path + "/debug.log");
+    f.open("w");
+    f.write(debug_log);
+}
+
+
+function  obo_af(in_af)
+{
+    var ob = mu.actionReferenceToObject(in_af);
+    var af = mu.objectToActionReference(ob);
+
+    if(mu.actionReferenceToJSON(in_af)!=mu.actionReferenceToJSON(af))
+    {
+       log("// ERR--------------------");
+    }
+
+   log(mu.actionReferenceToJSON(in_af));
+   log(mu.actionReferenceToJSON(af));
+
+}
+
+function  obo_ad(in_af)
+{
+    var ob = mu.actionDescriptorToObject(in_af);
+    var ad = mu.objectToActionDescriptor(ob);
+   log("//===============================");
+    if(mu.actionDescriptorToJSON(in_af)!=mu.actionDescriptorToJSON(ad))
+    {
+       log("//ERR--------------------");
+    }
+
+   log(mu.actionDescriptorToJSON(in_af));
+   log("//------");
+   log(mu.actionDescriptorToJSON(ad));
+
+}
+
+
+function see(ob)
+{
+
+    var str="\n"
+    for (var i  in ob)
+    {
+        try
+        {
+            str+=i +":" +ob[i]+"\n";
+        } catch (e)
+        {
+        }
+
+    }
+
+    alert(str)
+
+}
