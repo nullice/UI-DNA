03e949f539b50d3acc99f7d80dcd820bfeeca9db
nullice <ui@nullice.com>
2016-11-21 22:21:49 +0800

    + vue-color-cylinder

--- DVE/components/vue-color-cylinder/lib/color-range.vue
+++ DVE/components/vue-color-cylinder/lib/color-range.vue
@@ -1,24 +1,121 @@
 <template>
-    <div>
-        <input type="text" v-model="in_value"> [{{in_value}}],
-        <button v-on:click="in_value=333" > a d ds</button>
+    <div class="color-range">
+        <div class="range-title">{{range_title}}</div>
+        <div class="range-bar"
+             v-on:click="range_select($event)"
+        >
+            <div class="range-thumb" v-bind:style="rangeThumbStyle"></div>
+        </div>
+        <div class="range-input">
+            <input type="text" v-model="in_value">
+            <div class="spin-button">
+                <div class="spin-up"></div>
+                <div class="spin-down"></div>
+            </div>
+        </div>
+
     </div>
+
+    <!--<input type="text" v-model="in_value"> [{{in_value}}],-->
+    <button v-on:click="in_value=333"> a d ds</button>
 </template>
-<style>
-    body{
-        background-color:#ff0000;
+<style lang="scss">
+
+    .color-range {
+        white-space: nowrap;
+        .range-bar {
+            display: inline-block;
+            height: 10px;
+            min-width: 140px;
+            background: #3b3b3b;
+            position: relative;
+
+            .range-thumb {
+                width: 4px;
+                height: 8px;
+                background: #fff;
+                position: absolute;
+                left: 0px;
+                transition: 0.4s all;
+                border-radius: 1px;
+                margin: 1px 0;
+
+            }
+        }
+
+        .range-title {
+            display: inline-block;
+        }
+        .range-input {
+            width: 54px;
+            display: inline-block;
+
+            input {
+                width: 44px;
+            }
+
+        }
     }
+
 </style>
 <script>
 
     export default{
-        props: ['in_value',],
+        props: ['in_value', 'range_title', 'value_type'],
         data(){
-            return{
-                msg:'hello vue'
+            return {
+                msg: 'hello vue',
+                width: 140,
+                rangeThumbStyle: {
+                    left: "0px"
+                }
+
             }
         },
-        components:{
+        methods: {
+            range_select: function (e)
+            {
+
+                console.log("layerX", e.layerX, " e.layery", e.layerY)
+
+                var offsetX = e.layerX
+                var width = e.target.offsetWidth
+                this.width = width
+
+                if (offsetX < 10)
+                {
+                    offsetX = 0;
+                }
+                if (offsetX > width - 10)
+                {
+                    offsetX = width;
+                }
+
+
+                if (this.value_type == "hsl.h")
+                {
+
+                    this.in_value = ( offsetX / width) * 360
+                }
+                this.ramge_thumb_update();
+
+            },
+            ramge_thumb_update: function ()
+            {
+
+                var offsetX = this.in_value * this.width / 360
+                console.log("offsetX",offsetX)
+                if (offsetX > this.width - 5)
+                {
+                    this.rangeThumbStyle.left = offsetX - 5 + "px";
+                }
+                else
+                {
+                    this.rangeThumbStyle.left = offsetX + "px";
+                }
+
+            }
+
 
         }
     }
--- DVE/components/vue-color-cylinder/vue-color-cylinder.vue
+++ DVE/components/vue-color-cylinder/vue-color-cylinder.vue
@@ -5,20 +5,31 @@
         <div class="color-hsl">
             <div class="saturation-lighteness-picker-board"></div>
             <div class="hue-picker">
-                <color-range   v-bind:in_value.sync="color1.r"></color-range>
+                <color-range v-bind:in_value.sync="color1.hsl.h" range_title="H" value_type="hsl.h"> </color-range>
             </div>
         </div>
 
         <div class="color-rgb"></div>
 
-        <pre> {{color1|json 4}}</pre>
+        <!--<pre> {{color1|json 4}}</pre>-->
     </div>
 
 </template>
-<style>
-    body {
-        background-color: #ff0000;
+<style lang="scss">
+
+    .vue-color-cylinder-main-box {
+        position: absolute;
+        width: 240px;
+        height: 250px;
+        background: #fff;
+        z-index: 22;
+        border-radius: 4px;
+        box-shadow: 0 8px 12px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.08);
+        padding: 10px;
+        box-sizing: border-box;
     }
+
+
 </style>
 <script>
     import  IchiColor  from "./../../Caryon/IchiColor/ichi-color.js";
