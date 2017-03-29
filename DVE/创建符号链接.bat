0956694bf9a508e6d100eb23b8041e970242914c
unknown <>
2017-03-29 21:01:24 +0800

    [0]

--- DVE/创建符号链接.bat
+++ DVE/创建符号链接.bat
@@ -1,8 +1,8 @@
-mklink /d /j "E:\Work\GitHub\UI-DNA\DVE\bin\CSS\Ex-morphogen" "E:\Work\GitHub\Ex-morphogen\Ex-morphogen"
-mklink /d /j "E:\Work\GitHub\UI-DNA\DVE\Caryon\IchiColor" "E:\Work\GitHub\ichiColor\src"
+rem  mklink /d /j "E:\Work\GitHub\UI-DNA\DVE\bin\CSS\Ex-morphogen" "E:\Work\GitHub\Ex-morphogen\Ex-morphogen"
+rem  mklink /d /j "E:\Work\GitHub\UI-DNA\DVE\Caryon\IchiColor" "E:\Work\GitHub\ichiColor\src"
 
 
 
 
-mklink /d /j "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\UI-DNA" "E:\Work\GitHub\UI-DNA\DVE\bin"
+mklink /d /j "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\UI-DNA" "C:\Users\nullice\MyProject\UI-DNA\DVE\bin"
 pause
